618e917962e3b4eff0e8bd7ea55281a27ca721bf
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-11-17 23:45:40 +0500

    Task2

---
+++ Tyuiu.GrigorevKU.Sprint5.Task2.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorevKU.Sprint5.Task2.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorevKU.Sprint5.Task2.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4bc7a14-2014-4a7e-8289-79ab56fa69d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
