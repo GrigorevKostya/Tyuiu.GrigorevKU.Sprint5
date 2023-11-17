bc0f5aa61019aeb3e7a221e37376e49c6720102f
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-11-17 23:25:26 +0500

    Task1

---
+++ Tyuiu.GrigorevKU.Sprint5.Task1.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.GrigorevKU.Sprint5.Task1.V10.Lib;
+
+namespace Tyuiu.GrigorevKU.Sprint5.Task1.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            string path = @"C:\Users\dmitr\source\repos\Tyuiu.GrigorevKU.Sprint5\Tyuiu.GrigorevKU.Sprint5.Task1.V10\bin\Debug\OutPutFileTask1.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileexists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileexists);
+        }
+    }
+}
---
+++ Tyuiu.GrigorevKU.Sprint5.Task1.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorevKU.Sprint5.Task1.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorevKU.Sprint5.Task1.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fdd4e3c-5a33-46b1-bc47-9ad902dea532")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
