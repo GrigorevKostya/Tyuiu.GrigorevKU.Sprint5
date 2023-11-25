c4be2e6328a69036deca39fc1b658ae2cd5f2a65
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-11-25 16:24:20 +0500

    Task7

---
+++ Tyuiu.GrigorevKU.Sprint5.Task7.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorevKU.Sprint5.Task7.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorevKU.Sprint5.Task7.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f073f0d6-432b-48f4-aa4f-894c1ade4b65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GrigorevKU.Sprint5.Task7.V3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.GrigorevKU.Sprint5.Task7.V3.Lib;
+namespace Tyuiu.GrigorevKU.Sprint5.Task7.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Григорьев К.Ю. | ИИПб-23-2";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла.                                     *");
+            Console.WriteLine("* Задание #7                                                                   *");
+            Console.WriteLine("* Вариант #3                                                                   *");
+            Console.WriteLine("* Выполнил  Григорьев К.Ю. | ИИПб-23-2                                         *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дан файл в котором есть набор символьных данных. Удалить все русские буквы   *");
+            Console.WriteLine("* из файла.Полученный результат сохранить в файл.                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V3.txt";
+            string pathSaveFile = $@"C:\DataSprint5\OutPutDataFileTask7V3.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
